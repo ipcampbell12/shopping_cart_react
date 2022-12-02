07d3d28ba19ba4a62a4a7fad03634b883128ef43
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-02 09:25:19 -0800

    shopping cart icon

--- src/Components/LowerCart.js
+++ src/Components/LowerCart.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { numberFormatter } from '../App';
 import '../App.scss'
-
+import pic from './scpng.png'
 
 function LowerCart({ grandTotal, onClear }) {
     return (
         <div>
             <div className="lower-cart">
+                <div className="icon-container">
+                    <img src={pic} alt="Cart" className='icon' />
+                    <span className="quantity">1</span>
+                </div>
+
                 <p> Grand Total: <span id="grand-total"> {numberFormatter.format(grandTotal)}</span></p>
                 <button> Submit Order</button>
                 <button id='reset-button' onClick={onClear}> Reset Order</button>
